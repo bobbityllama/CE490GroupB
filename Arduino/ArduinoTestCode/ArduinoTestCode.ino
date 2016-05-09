18a63dbf81fd31dacf1e52e69ec24a972d170966
Arjun <arjunnamineni@gmail.com>
2016-05-09 18:05:38 -0400

    Integrated MongoDB

--- Arduino/ArduinoTestCode/ArduinoTestCode.ino
+++ Arduino/ArduinoTestCode/ArduinoTestCode.ino
@@ -1,8 +1,10 @@
-int buttonState = 0;
-String input;
+#include <ArduinoJson.h>
 
 int buttonPin = 46;
 int ledPin = 48;
+
+int buttonState = 0;
+int timeDelay = 50;
 
 void setup() {
   pinMode(ledPin,OUTPUT);
@@ -13,25 +15,66 @@ void setup() {
 
 void loop() {
   buttonState = digitalRead(buttonPin);
+  
+  StaticJsonBuffer<200> jsonBuffer;
+
+  JsonObject& SN1 = jsonBuffer.createObject();
+
+  SN1["id"] = "1";
+  SN1["lat"] = "-25.363";
+  SN1["lon"] = "31.044";
+  SN1["time"] = "0";
+
+  SN1.printTo(Serial);
+  Serial.write("\n");
+  delay(timeDelay);
 
   if(buttonState == HIGH)
   {
-    Serial.write("HIGH**");
+    JsonObject& SN2 = jsonBuffer.createObject();
+
+    SN2["id"] = "2";
+    SN2["lat"] = "-25.363";
+    SN2["lon"] = "1.044";
+    SN2["time"] = "999";
+  
+    SN2.printTo(Serial);
+    Serial.write("\n");
+    delay(timeDelay);
+
+    JsonObject& SN4 = jsonBuffer.createObject();
+
+    SN4["id"] = "4";
+    SN4["lat"] = "-25.363";
+    SN4["lon"] = "41.044";
+    SN4["time"] = "999";
+  
+    SN4.printTo(Serial);
+    Serial.write("\n");
+    delay(timeDelay);
   }
   else
   {
-    Serial.write("LOW**");
-  }
+    JsonObject& SN2 = jsonBuffer.createObject();
 
-  input = Serial.readString();
-
-  if (input.equals("99"))
-  {
-    digitalWrite(ledPin, LOW);
-  }
-  else if (input.equals("11"))
-  {
-    digitalWrite(ledPin, HIGH);
-  }
+    SN2["id"] = "2";
+    SN2["lat"] = "-25.363";
+    SN2["lon"] = "21.044";
+    SN2["time"] = "0";
   
+    SN2.printTo(Serial);
+    Serial.write("\n");
+    delay(timeDelay);
+  }
+
+  JsonObject& SN3 = jsonBuffer.createObject();
+
+  SN3["id"] = "3";
+  SN3["lat"] = "-25.363";
+  SN3["lon"] = "11.044";
+  SN3["time"] = "0";
+
+  SN3.printTo(Serial);
+  Serial.write("\n");
+  delay(timeDelay);
 }
--- index.js
+++ index.js
@@ -5,46 +5,208 @@ var io = require('socket.io')(app.listen(8081));
 var serialport = require('serialport');
 var SerialPort = serialport.SerialPort;
 
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var ObjectId = require('mongodb').ObjectID;
+
 app.use(express.static(__dirname + '/app'));
 app.use(express.static(__dirname + '/bower_components'));
+
+var router = express.Router();
 
 var serialport = new SerialPort("COM3", {
   baudrate: 9600,
   dataBits: 8,
   parity: 'none',
   stopBits: 1,
-  parser: serialport.parsers.readline('**')
+  parser: serialport.parsers.readline('\n')
 }, true);
 
 app.get('/', function (res) {
   res.sendfile('/index.html');
 });
 
+
+router.get('/test', function(req, res, next){
+  res.sendfile('/serverpage.html');
+});
+
+var url = 'mongodb://54.173.182.65/CE490GroupB';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
+
+var skipped = 0;
+
 serialport.on('open', function(){
   console.log('Serial Port Opened');
-  var lastValue;
+  //var lastValue;
 
   serialport.on('data', function(data){
-    if(lastValue != data.toString())
-    {
-      if(data.toString().indexOf("HIGH") > -1)
-      {
-        io.emit('high');
-      }
-      else
-      {
-        io.emit('low');
-      }
+    //console.log(data.toString());
+    var obj;
+    
+    try{
+      obj = JSON.parse(data);
+      obj.epochtime = Date.now();
+      obj.triggered = 0;
     }
-    lastValue = data.toString(); 
-  })
+    catch(e)
+    {
+      console.log("not json");
+      obj = null;
+    }
+    //console.log(obj);
+    if(obj != null){
+      
+
+      //console.log(obj);
+
+      MongoClient.connect(url, function(err, db) {
+        //assert.equal(null, err);
+        //console.log(db);
+        //console.log(obj);
+        //console.log("==================================");
+        if(db != null)
+        {
+          insertNodeData(db, function() {
+            db.close();
+          }, obj);
+        }
+        else
+        {
+          skipped++;
+          console.log(skipped);
+          db.close();
+        }
+        
+      });
+
+      MongoClient.connect(url, function(err, db) {
+        //assert.equal(null, err);
+        //console.log(db);
+        //console.log(obj);
+        //console.log("++++++++++++++++++++++++++++++++++");
+        if(db != null)
+        {
+          updateNodeData(db, function() {
+            db.close();
+          }, obj);
+        }
+        else
+        {
+          skipped++;
+          console.log(skipped);
+          db.close();
+        }
+        
+      });
+    }
+    
+  });
 
   io.on('connection', function(socket) {
       socket.on('lightoff', function() {
-        serialport.write("99");
+        //serialport.write("99");
       });
       socket.on('lighton', function() {
-        serialport.write("11");
+        //serialport.write("11");
+      });
+      
+      socket.on("init", function() {
+        MongoClient.connect(url, function(err, db) {
+          //assert.equal(null, err);
+          if(db != null)
+          {
+            findData(db, function() {
+              db.close();
+            });
+          }
+          else
+          {
+            skipped++;
+            console.log(skipped);
+          }
+        });
       });
   });
 });
+
+
+
+var insertNodeData = function(db, callback, nodedata) {
+   db.collection('NodeData').insertOne( nodedata, function(err, result) {
+    assert.equal(err, null);
+    //console.log("Inserted a document into the NodeData collection.");
+    callback();
+  });
+};
+
+
+var findData = function(db, callback) {
+   var cursor =db.collection('SingleNodeData').find( );
+   cursor.each(function(err, doc) {
+      assert.equal(err, null);
+      if (doc != null) {
+         io.emit('data', doc);
+      } else {
+         callback();
+      }
+   });
+};
+
+var updateNodeData = function(db, callback, nodedata) {
+  db.collection('SingleNodeData').findOne({"id":nodedata.id}, function(err, user){
+    if(user)
+    {
+      //console.log(user);
+      if(nodedata.time > 0)
+      {
+        nodedata.triggered = 1;
+        nodedata.previoustriggeredtime = nodedata.epochtime;
+      }
+      else if((Date.now() - user.previoustriggeredtime) < 1000)
+      {
+        nodedata.triggered = 1;
+        nodedata.previoustriggeredtime = user.previoustriggeredtime;
+        //console.log("2");
+      }
+
+      //console.log(Date.now() + "/" + nodedata.previoustriggeredtime + "/" + user.previoustriggeredtime);
+      //console.log(Date.now() - nodedata.previoustriggeredtime)
+
+      io.emit('data', nodedata);
+
+      //exists
+      //console.log(nodedata.id + " exists");
+      db.collection('SingleNodeData').updateOne(
+        {"id":nodedata.id},
+        {
+          $set: {"epochtime":nodedata.epochtime},
+          $set: { "lat": nodedata.lat },
+          $set: { "lon": nodedata.lon },
+          $set: { "time": nodedata.time },
+          $set: { "triggered": nodedata.triggered },
+          $set: { "previoustriggeredtime": nodedata.previoustriggeredtime }
+        }
+      , function(err, results) {
+          //console.log(results);
+          //console.log(nodedata);
+          callback();
+       });
+    }
+    else
+    {
+      //does not exist
+      //console.log(nodedata.id + " does not exist");
+      db.collection('SingleNodeData').insertOne( nodedata, function(err, result) {
+        assert.equal(err, null);
+        //console.log(nodedata);
+        callback();
+      });
+    }
+  });
+};
