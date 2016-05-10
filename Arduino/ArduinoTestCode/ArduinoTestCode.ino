089ddece9d5f9b54e5183759696bc336d26bfeb3
Arjun <arjunnamineni@gmail.com>
2016-05-10 09:57:38 -0400

    Fixed mongodb connection issue

--- Arduino/ArduinoTestCode/ArduinoTestCode.ino
+++ Arduino/ArduinoTestCode/ArduinoTestCode.ino
@@ -21,60 +21,60 @@ void loop() {
   JsonObject& SN1 = jsonBuffer.createObject();
 
   SN1["id"] = "1";
-  SN1["lat"] = "-25.363";
-  SN1["lon"] = "31.044";
+  SN1["lat"] = "43.010429";
+  SN1["lon"] = "-83.715630";
   SN1["time"] = "0";
 
   SN1.printTo(Serial);
   Serial.write("\n");
-  delay(timeDelay);
+  //delay(timeDelay);
 
   if(buttonState == HIGH)
   {
     JsonObject& SN2 = jsonBuffer.createObject();
 
     SN2["id"] = "2";
-    SN2["lat"] = "-25.363";
-    SN2["lon"] = "1.044";
+    SN2["lat"] = "43.010604";
+    SN2["lon"] = "-83.716253";
     SN2["time"] = "999";
   
     SN2.printTo(Serial);
     Serial.write("\n");
-    delay(timeDelay);
+    //delay(timeDelay);
 
     JsonObject& SN4 = jsonBuffer.createObject();
 
     SN4["id"] = "4";
-    SN4["lat"] = "-25.363";
-    SN4["lon"] = "41.044";
+    SN4["lat"] = "43.010201";
+    SN4["lon"] = "-83.716633";
     SN4["time"] = "999";
   
     SN4.printTo(Serial);
     Serial.write("\n");
-    delay(timeDelay);
+    //delay(timeDelay);
   }
   else
   {
     JsonObject& SN2 = jsonBuffer.createObject();
 
     SN2["id"] = "2";
-    SN2["lat"] = "-25.363";
-    SN2["lon"] = "21.044";
+    SN2["lat"] = "43.009571";
+    SN2["lon"] = "-83.715563";
     SN2["time"] = "0";
   
     SN2.printTo(Serial);
     Serial.write("\n");
-    delay(timeDelay);
+    //delay(timeDelay);
   }
 
   JsonObject& SN3 = jsonBuffer.createObject();
 
   SN3["id"] = "3";
-  SN3["lat"] = "-25.363";
-  SN3["lon"] = "11.044";
+  SN3["lat"] = "43.009568";
+  SN3["lon"] = "-83.718836";
   SN3["time"] = "0";
 
   SN3.printTo(Serial);
   Serial.write("\n");
-  delay(timeDelay);
+  //delay(timeDelay);
 }
--- index.js
+++ index.js
@@ -27,26 +27,16 @@ app.get('/', function (res) {
   res.sendfile('/index.html');
 });
 
-
-router.get('/test', function(req, res, next){
-  res.sendfile('/serverpage.html');
-});
+var globalDB;
 
 var url = 'mongodb://54.173.182.65/CE490GroupB';
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  db.close();
+  globalDB = db;
 });
 
-var skipped = 0;
-
 serialport.on('open', function(){
-  console.log('Serial Port Opened');
-  //var lastValue;
-
   serialport.on('data', function(data){
-    //console.log(data.toString());
     var obj;
     
     try{
@@ -56,157 +46,103 @@ serialport.on('open', function(){
     }
     catch(e)
     {
-      console.log("not json");
       obj = null;
     }
-    //console.log(obj);
-    if(obj != null){
-      
-
-      //console.log(obj);
-
-      MongoClient.connect(url, function(err, db) {
-        //assert.equal(null, err);
-        //console.log(db);
-        //console.log(obj);
-        //console.log("==================================");
-        if(db != null)
-        {
-          insertNodeData(db, function() {
-            db.close();
-          }, obj);
-        }
-        else
-        {
-          skipped++;
-          console.log(skipped);
-          db.close();
-        }
-        
-      });
-
-      MongoClient.connect(url, function(err, db) {
-        //assert.equal(null, err);
-        //console.log(db);
-        //console.log(obj);
-        //console.log("++++++++++++++++++++++++++++++++++");
-        if(db != null)
-        {
-          updateNodeData(db, function() {
-            db.close();
-          }, obj);
-        }
-        else
-        {
-          skipped++;
-          console.log(skipped);
-          db.close();
-        }
-        
-      });
+    if(obj != null && globalDB != undefined){
+      insertNodeData(globalDB, obj);
+      updateNodeData(globalDB, obj);
     }
     
   });
 
   io.on('connection', function(socket) {
-      socket.on('lightoff', function() {
-        //serialport.write("99");
-      });
-      socket.on('lighton', function() {
-        //serialport.write("11");
-      });
-      
-      socket.on("init", function() {
-        MongoClient.connect(url, function(err, db) {
-          //assert.equal(null, err);
-          if(db != null)
-          {
-            findData(db, function() {
-              db.close();
-            });
-          }
-          else
-          {
-            skipped++;
-            console.log(skipped);
-          }
-        });
-      });
+    socket.on("init", function() {
+      findData(globalDB);
+      findTriggeredData(globalDB);
+    });
+    socket.on("cleardata", function() {
+      globalDB.collection('TriggeredData').remove();
+    });
   });
 });
 
 
 
-var insertNodeData = function(db, callback, nodedata) {
+var insertNodeData = function(db, nodedata) {
    db.collection('NodeData').insertOne( nodedata, function(err, result) {
     assert.equal(err, null);
-    //console.log("Inserted a document into the NodeData collection.");
-    callback();
   });
 };
 
 
-var findData = function(db, callback) {
+var findData = function(db) {
    var cursor =db.collection('SingleNodeData').find( );
    cursor.each(function(err, doc) {
       assert.equal(err, null);
       if (doc != null) {
          io.emit('data', doc);
-      } else {
-         callback();
       }
    });
 };
 
-var updateNodeData = function(db, callback, nodedata) {
+var findTriggeredData = function(db) {
+   var cursor =db.collection('TriggeredData').find( );
+   cursor.each(function(err, doc) {
+      assert.equal(err, null);
+      if (doc != null) {
+         io.emit('triggereddata', doc);
+         console.log(doc);
+      }
+   });
+};
+
+var updateNodeData = function(db, nodedata) {
   db.collection('SingleNodeData').findOne({"id":nodedata.id}, function(err, user){
     if(user)
     {
-      //console.log(user);
+      //exists
       if(nodedata.time > 0)
       {
         nodedata.triggered = 1;
         nodedata.previoustriggeredtime = nodedata.epochtime;
+        insertTriggeredData(globalDB, nodedata);
+        io.emit('triggereddata', nodedata);
       }
-      else if((Date.now() - user.previoustriggeredtime) < 1000)
+      else if((Date.now() - user.previoustriggeredtime) < 5000)
       {
         nodedata.triggered = 1;
         nodedata.previoustriggeredtime = user.previoustriggeredtime;
-        //console.log("2");
       }
 
-      //console.log(Date.now() + "/" + nodedata.previoustriggeredtime + "/" + user.previoustriggeredtime);
-      //console.log(Date.now() - nodedata.previoustriggeredtime)
-
+      // put in "does not exist" condition as well
       io.emit('data', nodedata);
 
-      //exists
-      //console.log(nodedata.id + " exists");
       db.collection('SingleNodeData').updateOne(
         {"id":nodedata.id},
         {
-          $set: {"epochtime":nodedata.epochtime},
-          $set: { "lat": nodedata.lat },
-          $set: { "lon": nodedata.lon },
-          $set: { "time": nodedata.time },
-          $set: { "triggered": nodedata.triggered },
-          $set: { "previoustriggeredtime": nodedata.previoustriggeredtime }
-        }
-      , function(err, results) {
-          //console.log(results);
-          //console.log(nodedata);
-          callback();
-       });
+          $set: 
+          {
+            "epochtime":nodedata.epochtime,
+            "lat": nodedata.lat,
+            "lon": nodedata.lon,
+            "time": nodedata.time,
+            "triggered": nodedata.triggered,
+            "previoustriggeredtime": nodedata.previoustriggeredtime
+          }
+      });
     }
     else
     {
       //does not exist
-      //console.log(nodedata.id + " does not exist");
       db.collection('SingleNodeData').insertOne( nodedata, function(err, result) {
         assert.equal(err, null);
-        //console.log(nodedata);
-        callback();
       });
     }
+  });
+};
+
+var insertTriggeredData = function(db, nodedata) {
+   db.collection('TriggeredData').insertOne( nodedata, function(err, result) {
+    assert.equal(err, null);
   });
 };
